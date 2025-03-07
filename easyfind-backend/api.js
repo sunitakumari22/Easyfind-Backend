47f200b44c3ba4dc023d4e77717efc92f47b5bb6
SunitaKumari <sunita.kumari@sbu.ac.in>
2025-03-07 14:03:45 +0530

    Created post ,put and delete Api

--- easyfind-backend/api.js
+++ easyfind-backend/api.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const dbConnect = require('./mongodb')
 const app = express();
+const { ObjectId } = require('mongodb');
 
+
+app.use(express.json())
 
 app.get('/', async (req, res) => {
     let data =await dbConnect();
@@ -9,4 +12,25 @@ app.get('/', async (req, res) => {
     console.log(data)
     res.send(data)
 })
+
+app.post('/', async  (req, res) => {
+    console.log(req.body)
+    let data= await dbConnect();
+    let result= await data.insertOne(req.body);
+    res.send(result)
+})
+app.put('/:name', async  (req, res) => {
+    let data =await dbConnect();
+    let result= await data.updateOne(
+        { name:req.params.name},
+        {$set:req.body}
+    )
+    res.send("result coming");
+})
+app.delete('/:id', async (req, res) => {
+    console.log(req.params.id);
+    const data = await dbConnect();
+    let result = await data.deleteOne({ _id: new ObjectId(req.params.id) }); 
+    res.send(result);
+});
 app.listen(5000);
