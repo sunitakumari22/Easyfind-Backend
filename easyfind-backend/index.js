f1298037ce838a734a8d81d3037a6d92c61e2c6c
SunitaKumari <sunita.kumari@sbu.ac.in>
2025-03-07 16:31:18 +0530

    Post Api using mongoose

--- easyfind-backend/index.js
+++ easyfind-backend/index.js
@@ -1,42 +1,16 @@
-// const dbConnect=require('./mongodb') 
+const express = require('express');
+require('./config');
+const user=require('./users');
 
+const app=express();
+app.use(express.json());
 
-// // first step to handle promises
-//  dbConnect().then((resp)=>{
-//     resp.find({}).toArray().then((data)=>{
-//         console.log(data)
-//     })
-//  })
+app.post("/create",async (req,res)=>{
+    let data=new user(req.body)
+    let result= await data.save()
+    console.log(req.body);    
+    res.send(req.body)
+})
+app.listen(5000);
 
-// //  second step to handle promises
-// // const main= async()=>{
-// //     console.log("main function called");
-// //     let data=  await dbConnect();
-// //     data= await data.find().toArray();
-// //     console.warn(data);   
-    
-// // }
-// // main();
-
-const mongoose=require('mongoose');
-
-
-const main= async()=>{
-    await mongoose.connect("mongodb://localhost:27017/easyfind-backend");
-     const userSch=new mongoose.Schema({
-        name:String,
-        email:String,
-        mobile:String,
-        password:String
-     });
-
- const usersModel=mongoose.model('users',userSch);
- let data =new   usersModel({name:"siya",email:'siya@gmail.com'})  ;
- let result = await data.save();
- console.log(result);
- 
-
-}
-
-main()
 
