72eb4840053518ea676c1c01abeed3aff0c7e736
SunitaKumari <sunita.kumari@sbu.ac.in>
2025-03-07 16:48:04 +0530

    Added get and delete Apis

--- easyfind-backend/index.js
+++ easyfind-backend/index.js
@@ -11,6 +11,25 @@ app.post("/create",async (req,res)=>{
     console.log(req.body);    
     res.send(req.body)
 })
+
+app.get("/list",async (req,res)=>{
+    let data= await user.find();
+    res.send(data);
+})
+
+app.delete("/delete/:_id",async (req,res)=>{
+    console.log(req.params);
+    let data= await user.deleteOne(req.params);
+    res.send(data);
+})
+// app.put('/update/:_id', async  (req, res) => {
+//     console.log(req.params);
+//     let result= await data.updateOne(
+//         { _id:req.params._id},
+//         {$set:req.body}
+//     )
+//     res.send(result);
+// })
 app.listen(5000);
 
 
